928c119be37dfc6cb892ae67d9b930a32cc1f51d
abdellah <abdo.abdell.2000@gmail.com>
2025-05-21 14:51:27 +0100

    implementer la logique de BlogController

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\BlogPost;
+use App\Models\Category;
 use App\Models\Event;
 use App\Models\Page;
 use App\Models\Service;
@@ -17,6 +18,7 @@ class DashboardController extends Controller
             'services' => Service::count(),
             'blog_posts' => BlogPost::count(),
             'events' => Event::count(),
+            'categories' => Category::count()
         ];
 
         return view('admin.dashboard', compact('stats'));
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
 <?php
 
 use App\Http\Controllers\Admin\BlogPostController;
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\EventController;
 use App\Http\Controllers\Admin\PageController;
 use App\Http\Controllers\Admin\ServiceController;
 use App\Http\Controllers\Admin\SettingController;
 use App\Http\Controllers\Admin\ZoneController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/blog', [HomeController::class, 'blog'])->name('blog');
+Route::get('/blog/{slug}', [HomeController::class, 'post'])->name('post');
+Route::get('/blog/category/{slug}', [HomeController::class, 'category'])->name('blog.category');
 
 require __DIR__.'/auth.php';
 
@@ -34,10 +39,14 @@ Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
 
     Route::get('/pages', [PageController::class, 'index'])->name('pages.index');
 
-
-    Route::get('/blog', [BlogPostController::class, 'index'])->name('blog.index');
-
-
     Route::get('/event', [EventController::class, 'index'])->name('event.index');
+
+
+    Route::resource('blog', BlogPostController::class);
+    Route::patch('blog/{post}/toggle-publish', [BlogPostController::class, 'togglePublish'])->name('blog.toggle-publish');
+    Route::get('blog/{post}/preview', [BlogPostController::class, 'preview'])->name('blog.preview');
+    
+    Route::resource('categories', CategoryController::class);
+
 
 });
